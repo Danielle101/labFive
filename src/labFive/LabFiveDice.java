9df8643003960107a1e5cdbb7e91c8c2026a9ae8
Danielle <halldan4@yahoo.com>
2017-01-19 08:22:44 -0500

    Update LabFiveDice.java

--- src/labFive/LabFiveDice.java
+++ src/labFive/LabFiveDice.java
@@ -7,9 +7,7 @@ static Scanner sc = new Scanner (System.in);
 	public static void main(String[] args) {
 //welcome the user to the game and ask if they would like to play
 		System.out.println("Welcome to the Grand Circus Casino! Roll the dice? (y/n) :");
-		
 		String choice2 = Validator.getString("Roll the dice? (y/n) :");
-		
 				while (choice2.equalsIgnoreCase("y"))
 				{	
 //if the user inputs "y" then ask how many faces they'd like on their dice
@@ -20,7 +18,6 @@ static Scanner sc = new Scanner (System.in);
 // generate the random number for dieOne and dieTwo using the dieUno method by using the formula random1.nextInt(diceNumber)+1; 
 		int dieOne = dieUno(diceNumber, random1);
 		int dieTwo = dieUno(diceNumber, random1);
-		
 //calculate the sum of dieOne and dieTwo. if the user rolls two ones then a message saying "You rolled Snake Eyes!" will appear
 				int addRandoms = dieOne + dieTwo;
 				if (addRandoms <= 2){
@@ -32,9 +29,7 @@ static Scanner sc = new Scanner (System.in);
 					choice2 = sc.nextLine();
 				}
 //if the user does not rolls two ones then a message displaying the two numbers they rolled will appear
-				else {
-				System.out.println ("Roll 1: ");
-				}
+				else {System.out.println ("Roll 1: ");}
 				System.out.println (dieOne); 
 				System.out.println (dieTwo);
 				choice2 = Validator.getString("Roll again? (y/n)");
@@ -42,8 +37,6 @@ static Scanner sc = new Scanner (System.in);
 //if the user does not want to play the game at all or does not want to play again display "Bye!".
 				System.out.println("Bye!");
 		}
-		
-
 	/**
 	 * @param diceNumber
 	 * @param random1
